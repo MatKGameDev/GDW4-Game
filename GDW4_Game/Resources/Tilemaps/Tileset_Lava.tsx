ae21a5e931b9bed02309ae82542a068ea7ca7aa8
Marcus Vine <marcus.vine@uoit.net>
2019-03-09 19:33:42 -0500

    [fixing test]

---
+++ GDW4_Game/Resources/Tilemaps/Tileset_Lava.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Tileset Lava" tilewidth="64" tileheight="64" tilecount="9" columns="3">
+ <image source="Tileset_Lava.png" width="192" height="192"/>
+</tileset>
---
+++ GDW4_Game/Resources/Tilemaps/top_right.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="top right" tilewidth="64" tileheight="64" tilecount="9" columns="3">
+ <image source="TOP_RIGHT.png" width="192" height="192"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="0" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
