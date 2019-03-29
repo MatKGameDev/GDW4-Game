c225f1f3f3192e7e64c5057dc76a0d2972864589
MK <mathew.kostrzewa@uoit.net>
2019-03-29 14:41:44 -0400

    [FIX]
    fixed crashing due to animation preloading

--- GDW4_Game/Classes/MainMenu.cpp
+++ GDW4_Game/Classes/MainMenu.cpp
@@ -101,7 +101,7 @@ void MainMenu::preloadAnimations()
 {
 	//flamethrower attack
 	auto sprite = cocos2d::Sprite::create("Sprites/flame_sprite.png");
-	auto anim = cocos2d::AnimationCache::getInstance()->getAnimation("boss_flame_animation_key");
+	auto anim = cocos2d::AnimationCache::getInstance()->getAnimation("boss_flame_tell_PRE_animation_key");
 	auto action = cocos2d::Animate::create(anim);
 	sprite->stopAllActions();
 	sprite->runAction(cocos2d::Repeat::create(action->clone(), 1));
