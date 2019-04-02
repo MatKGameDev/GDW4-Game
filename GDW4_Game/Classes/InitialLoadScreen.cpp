98cb8a75027e59d056e784d836e1ceb550375c33
MK <mathew.kostrzewa@uoit.net>
2019-04-02 00:57:06 -0400

    [ADD]
    started adding initial load screen

---
+++ GDW4_Game/Classes/InitialLoadScreen.cpp
@@ -0,0 +1,12 @@
+#include "InitialLoadScreen.h"
+
+
+
+InitialLoadScreen::InitialLoadScreen()
+{
+}
+
+
+InitialLoadScreen::~InitialLoadScreen()
+{
+}
---
+++ GDW4_Game/Classes/InitialLoadScreen.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef INITIALLOADSCENE_H
+#define INITIALLOADSCENE_H
+
+#include "cocos2d.h"
+using namespace cocos2d;
+class InitialLoadScreen : public cocos2d::Scene
+{
+public:
+	InitialLoadScreen();
+	~InitialLoadScreen();
+};
+#endif
