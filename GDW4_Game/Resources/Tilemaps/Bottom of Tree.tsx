4dc4532be366c9f88ad5611a18d1e66ee545c36c
Unknown <jordan.mcgill@uoit.net>
2019-04-02 03:14:41 -0400

    Test me bois

---
+++ GDW4_Game/Resources/Tilemaps/Bottom of Tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Bottom of Tree" tilewidth="64" tileheight="64" tilecount="10" columns="5">
+ <image source="bottomoftree.png" width="320" height="128"/>
+</tileset>
